f761ea0b825aebbb251491e053e102b9e39b75cc
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-02-10 22:46:57 +0000

    objects

--- members.js
+++ members.js
@@ -1,3 +1,18 @@
-const members = [];
+const members = [
+{ firstName: "John",
+lastName: "Doe",
+jobTitle: "Software Engineer",
+department: "Engineering",
+manager: null,
+locatio: "Glasgow",
+firstAider: false,
+userSlack: "johndoe",
+timezone: "America/Los_Angeles"
+}
+
+
+
+
+];
 
 module.exports = members;
