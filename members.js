39a2870e4578b4302fa7feb80316b33c366ccaa4
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-02-11 19:02:55 +0000

    bject x4

--- members.js
+++ members.js
@@ -1,17 +1,45 @@
 const members = [
-{ firstName: "John",
-lastName: "Doe",
-jobTitle: "Software Engineer",
-department: "Engineering",
-manager: null,
-locatio: "Glasgow",
-firstAider: false,
-userSlack: "johndoe",
-timezone: "America/Los_Angeles"
-}
-
-
-
+    { firstName: "John",
+    lastName: "Doe",
+    jobTitle: "Software Engineer",
+    department: "Engineering",
+    manager: null,
+    locatio: "Glasgow",
+    firstAider: false,
+    userSlack: "johndoe",
+    timezone: "America/Los_Angeles"
+    },
+    { firstName: "Jane",
+    lastName: "Doe",
+    jobTitle: "Software Engineer",
+    department: "Engineering",
+    manager: "John Doe",
+    locatio: "Leeds",
+    firstAider: false,
+    userSlack: "janedoe",
+    timezone: "America/Los_Angeles"
+    },
+    { firstName: "Bob",
+    lastName: "Doe",
+    jobTitle: "Software Engineer",
+    department: "Engineering",
+    manager: "Jane Doe",
+    locatio: "Cape Town",
+    firstAider: false,
+    userSlack: "bobdoe",
+    timezone: "America/Los_Angeles"
+    },
+    
+    { firstName: "Sally",
+    lastName: "Doe",
+    jobTitle: "Software Engineer",
+    department: "Engineering",
+    manager: "Jane Doe",
+    locatio: "Rome",
+    firstAider: false,
+    userSlack: "sallydoe",
+    timezone: "America/Los_Angeles"
+    }
 
 ];
 
